84e1923dcfb082771eda58d7fad0f64a54263e08
md-jahirul-islam-tuku <tukuwebian@gmail.com>
2025-12-17 16:38:51 +0300

    home page created done

--- src/pages/CategoryNews/CategoryNews.jsx
+++ src/pages/CategoryNews/CategoryNews.jsx
@@ -1,17 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router";
 import CategoryNewsCard from "./CategoryNewsCard";
 
 const CategoryNews = () => {
+  const [categories, setCategories] = useState([]);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const categoriesRes = await fetch("../../../public/categories.json");
+        const categoriesData = await categoriesRes.json();
+        setCategories(categoriesData);
+      } catch {
+        setError("Something went wrong");
+        console.log(error);
+      }
+    })();
+  }, [error]);
+
   const { id } = useParams();
   const data = useLoaderData();
+  const categoriesTitle = categories.find((item) => item.id === Number(id));
+  console.log(categoriesTitle?.name);
   const news =
     id === "0" ? data : data.filter((news) => news.category_id === Number(id));
   return (
     <div className="col-span-6">
       <h1 className="text-2xl text-secondary font-semibold mb-5">
-        Dragon News Home {news.length}
+        {categoriesTitle?.name} News ({news.length})
       </h1>
+      <div>{}</div>
       <div>
         {news.map((item) => (
           <CategoryNewsCard key={item.id} item={item} />
--- src/pages/CategoryNews/CategoryNewsCard.jsx
+++ src/pages/CategoryNews/CategoryNewsCard.jsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { CiBookmark, CiShare2 } from "react-icons/ci";
+import { FaEye, FaStar } from "react-icons/fa";
+import { FiBookmark } from "react-icons/fi";
+import { IoMdShare } from "react-icons/io";
 
 const CategoryNewsCard = ({ item }) => {
   const {
@@ -16,18 +20,44 @@ const CategoryNewsCard = ({ item }) => {
     production,
   } = item;
   return (
-    <div className="card bg-base-100 shadow-sm">
-      <h2 className="card-title">{title}</h2>
-      <figure>
-        <img src={image_url} alt={`News_Image${title}`} />
+    <div className="card bg-base-100 shadow-sm mb-5">
+      <div className="bg-neutral flex justify-between items-center px-5 py-3 rounded-t-lg">
+        <div className="flex items-center gap-3">
+          <img className="rounded-full w-14" src={author.img} alt="" />
+          <div>
+            <h4 className="text-xl font-bold text-secondary">{author.name}</h4>
+            <p className="text-primary">{author.published_date}</p>
+          </div>
+        </div>
+        <div className="flex items-center gap-1 text-4xl">
+          <FiBookmark />
+          <IoMdShare />
+        </div>
+      </div>
+      <h2 className="card-title text-2xl p-5">{title}</h2>
+      <figure className="px-5">
+        <img
+          className="w-full rounded-lg"
+          src={image_url}
+          alt={`News_Image${title}`}
+        />
       </figure>
       <div className="card-body">
-        <p>
-          A card component has a figure, a body part, and inside body there are
-          title and actions parts
-        </p>
-        <div className="card-actions justify-end">
-          <button className="btn btn-primary">Buy Now</button>
+        <p>{details}</p>
+        <hr className="border-neutral border-2 border-dashed my-2" />
+        <div className="flex items-center justify-between">
+          <div className="flex gap-1 items-center">
+            <FaStar className="text-amber-500" />
+            <FaStar className="text-amber-500" />
+            <FaStar className="text-amber-500" />
+            <FaStar className="text-amber-500" />
+            <FaStar className="text-amber-500" />
+            <h4 className="text-lg">{rating.number}</h4>
+          </div>
+          <div className="flex items-center gap-2 text-xl">
+            <FaEye />
+            <h3>{total_view}</h3>
+          </div>
         </div>
       </div>
     </div>
